5da79cc2e804e3526e948cdb77ba6fd72e9d5d15
Wills Blake <richwblake@gmail.com>
2019-10-15 03:44:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -70,6 +70,13 @@ def directors_totals(nds)
     movie_index += 1
   end
   
+  movie_index = 0
+  
+  while movie_index < nds[8][:movies].length do
+    result["Robert Zemeckis"] += nds[8][:movies][movie_index][:worldwide_gross]
+    movie_index += 1
+  end
+  
   
   result
 end
