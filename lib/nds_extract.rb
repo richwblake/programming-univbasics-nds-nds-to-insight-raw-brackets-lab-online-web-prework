bd6ff3f467f1b277e8f9c7423c621162d0256488
Wills Blake <richwblake@gmail.com>
2019-10-15 03:43:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,6 +49,13 @@ def directors_totals(nds)
     movie_index += 1
   end
   
+  movie_index = 0
+  
+  while movie_index < nds[5][:movies].length do
+    result["Wachowski Siblings"] += nds[5][:movies][movie_index][:worldwide_gross]
+    movie_index += 1
+  end
+  
   
   result
 end
